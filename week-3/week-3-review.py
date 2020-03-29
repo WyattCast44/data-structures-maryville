ab3375d941b44201c925559336b50e9d6c32aa67
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2020-03-29 13:31:00 -0700

    wip

--- week-3/week-3-review.py
+++ week-3/week-3-review.py
@@ -11,7 +11,75 @@ def list_bounds(sequence: list):
 
     return sequence[0], sequence[-1]
 
-# 2. Write a recursive function to reverse a list.
+
+def list_bounds_recursive(sequence: list):
+
+    if len(sequence) == 0:
+
+        # No items in sequence, just return it
+        return sequence
+
+    if len(sequence) == 1:
+
+        # Sequence only has one item, it is both the min and the max
+        return sequence[0], sequence[0]
+
+    # Sequence has more than one item
+    # We need to recusively determine the min and max
+    # I chose to wrap the recursive function inside the
+    # main function because there is implementation details
+    # that end end use does not care about
+
+    def find_min_max(sequence, start, end):
+
+        end = int(end)
+        start = int(start)
+
+        # First we will test the case
+        # where the end index is less than or
+        # equal to the starting index
+        # in this case we have one or two
+        # items total
+        if end <= start + 1:
+
+            if sequence[start] < sequence[end]:
+                # If the starting index value is less than
+                # the end value then we have our min and max
+                return sequence[start], sequence[end]
+            else:
+                # If the above is not true, then we have
+                # one item and it is both the min and max
+                return sequence[end], sequence[start]
+
+        # Okay we know we have at least three items in the
+        # sequence, we will get the middle index
+        mid = (start+end)/2
+
+        # Now we split the sequence into two sequences divided by the
+        # middle index and we will find recursivly get each part down
+        # to the point where each seq will only have two items and
+        # we can then easily determine the min and max value
+        (left_min, left_max) = find_min_max(sequence, start, mid)
+        (right_min, right_max) = find_min_max(sequence, mid + 1, end)
+
+        # We will assume that the left side of the sequence
+        # had both the min and the max values
+        min_value = left_min
+        max_value = left_max
+
+        # We will now check those assumptions and
+        # compare values with the right side and
+        # correct min/max as needed
+        if right_max > max_value:
+            max_value = right_max
+        if right_min < min_value:
+            min_value = right_min
+
+        return min_value, max_value
+
+    return find_min_max(sequence, 0, len(sequence) - 1)
+
+# 2. Write a recursive function to reverse a list
 
 
 def reverse_list(sequence: list):
@@ -62,4 +130,4 @@ def reverse_list_recursive(sequence: list):
     # the last item of the original list
     # So the recursive function will return the
     # last item of the shortended list
-    return [sequence[-1]] + reverse_listss(sequence[:-1])
+    return [sequence[-1]] + reverse_list_recursive(sequence[:-1])
